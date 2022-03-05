d205d1771ea5246382bdbdda1ebc5828fde97e77
Jack Greenberg <kcaj@grebneerg.com>
2022-03-05 17:39:29 -0500

    Add toGdxColor to DaleColor

--- core/src/edu/cornell/gdiac/physics/platform/DaleColor.java
+++ core/src/edu/cornell/gdiac/physics/platform/DaleColor.java
@@ -1,7 +1,22 @@
 package edu.cornell.gdiac.physics.platform;
 
+import com.badlogic.gdx.graphics.Color;
+
 public enum DaleColor {
     RED,
     YELLOW,
     BLUE;
+
+    public Color toGdxColor() {
+        switch (this) {
+            case RED:
+                return Color.RED;
+            case BLUE:
+                return Color.BLUE;
+            case YELLOW:
+                return Color.YELLOW;
+            default:
+                return Color.WHITE;
+        }
+    }
 }
