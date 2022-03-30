f1dfaaa02a112d4f37e3808e030b124b0bd3aff2
Jack Greenberg <kcaj@grebneerg.com>
2022-03-30 19:20:30 -0400

    Some tiled configuration and extensions

---
+++ level-editor/disco.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="disco" tilewidth="32" tileheight="32" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="tiles/wall.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="tiles/reflective.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="tiles/purple.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="tiles/pink.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="tiles/orange.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="tiles/green.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="tiles/blue.png"/>
+ </tile>
+</tileset>
